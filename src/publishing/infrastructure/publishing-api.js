df0333a6d3784cc6d2476d7edd1eca800435413f
Leonardo Cabrera <u202415820@upc.edu.pe>
2026-04-27 12:09:18 -0500

    feat(publishing): add publishing ap.

---
+++ src/publishing/infrastructure/publishing-api.js
@@ -0,0 +1,28 @@
+import {BaseApi} from "../../shared/infrastructure/base-api.js";
+import {BaseEndpoint} from "../../shared/infrastructure/base-endpoint.js";
+
+const categoriesEndpointPath = import.meta.env.VITE_CATEGORIES_ENDPOINT_PATH;
+export class PublishingApi extends BaseApi {
+    #categoriesEndpoint;
+
+    constructor() {
+        super();
+        this.#categoriesEndpoint = new BaseEndpoint(this, categoriesEndpointPath);
+    }
+
+    getCategories() {
+        return this.#categoriesEndpoint.getAll();
+    }
+
+    getCategoryById(id) {
+        return this.#categoriesEndpoint.getById(id);
+    }
+
+    updateCategory(id, resource) {
+        return this.#categoriesEndpoint.update(id, resource);
+    }
+
+    deleteCategory(id) {
+        return this.#categoriesEndpoint.delete(id);
+    }
+}
